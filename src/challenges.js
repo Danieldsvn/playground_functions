90b2cf7a93c32dbcfe3c679d76fd773f6e45c094
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-02 19:12:46 -0300

    requisito 7 concluido

--- src/challenges.js
+++ src/challenges.js
@@ -78,9 +78,20 @@ function highestCount(array) {
 console.log(highestCount([1,1,1]));
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let cat1Distance = cat1 - mouse;
+  let cat2Distance = cat2 - mouse;
+  if (cat1Distance === cat2Distance || -cat1Distance === cat2Distance || cat1Distance === -cat2Distance)  {
+    return "os gatos trombam e o rato foge";
+  }
+  if (cat1Distance < cat2Distance ) {
+    return "cat1";
+  }
+  if (cat2Distance < cat1Distance ) {
+    return "cat2";
+  }
 }
+console.log(catAndMouse(2, 3, 4));
 
 // Desafio 8
 function fizzBuzz() {
