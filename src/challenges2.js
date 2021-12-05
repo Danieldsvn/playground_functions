2627a100dd51af5ddc7ca38eb38c957bef08e7f6
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-05 19:38:57 -0300

    Requisito 10 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,17 +1,23 @@
 // Desafio 10
 function techList(tech, name) {
-  if (tech === null) {
-  techOrdejado = tech.sort();
-  return 'Vazio!';
-}
-else {
-  
-  for ( i = 0; i < tech.length; i += 1) {
-
+  let techOrdenado = tech.sort();
+  let techList1 = new Object();
+  let arrayFinal = [];
+  if (tech.length === 0) { 
+   return 'Vazio!';
+  }    
+  for (let i = 0; i < techOrdenado.length; i += 1) {
+    techList1.tech = techOrdenado[i];
+    techList1.name = name;
+    arrayFinal.push(techList1);    
+    techList1 = new Object; // Débora Serra turma 19 B ajudou na solução de um pequeno problema no for.
   }
-}
+  
+return arrayFinal;
 
 }
+
+// console.log(techList(["React", "Javascript", "Node"], 'Daniel'));
 
 // Desafio 11
 function generatePhoneNumber() {
@@ -34,3 +40,4 @@ module.exports = {
   hydrate,
   triangleCheck,
 };
+
