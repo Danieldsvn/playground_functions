444afab1c8fb4468bf2095c2c717b36be7d08cd6
Daniel Saraiva <danieldsvn@gmail.com>
2021-12-05 21:41:48 -0300

    requisito 11 concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -17,11 +17,38 @@ return arrayFinal;
 
 }
 
-// console.log(techList(["React", "Javascript", "Node"], 'Daniel'));
 
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+  if (phoneNumber.length !== 11)
+  {
+    return 'Array com tamanho incorreto.';
+  }
+  for (let i = 0; i < phoneNumber.length; i += 1)
+  {
+    if(phoneNumber[i] < 0 || phoneNumber[i] > 9) 
+    {
+      return "não é possível gerar um número de telefone com esses valores";
+    }    
+  }
+  let contNumbers = 0;
+  for (let i = 0; i < phoneNumber.length; i += 1) 
+  {
+    contNumbers = 0;    
+    for (let j = 0; j < phoneNumber.length; j += 1)
+    {
+      if (phoneNumber[i] == phoneNumber[j])
+      {
+        contNumbers += 1;
+      }
+      if (contNumbers >= 3)
+      {
+        return "não é possível gerar um número de telefone com esses valores";
+      }
+    }
+  }
+  return '(' + phoneNumber[0] + phoneNumber[1] + ')' + ' ' + phoneNumber[2] + phoneNumber[3] + phoneNumber[4] + phoneNumber[5] + phoneNumber[6] + '-' + phoneNumber[7] + phoneNumber[8] + phoneNumber[9] + phoneNumber[10];
+  
 }
 
 // Desafio 12
